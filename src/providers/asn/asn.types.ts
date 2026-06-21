61cfdbe4dbb9f1632662c7672ea77d9de494db2e
J3r3mC <j.corbeilla@outlook.fr>
2026-06-21 19:04:36 +0200

    add providers asn

---
+++ src/providers/asn/asn.types.ts
@@ -0,0 +1,3 @@
+export interface AsnResult {
+  asn: number | null;
+}
---
+++ tests/asn/ip2location-asn.provider.test.ts
@@ -0,0 +1,15 @@
+jest.mock("ip2location-nodejs", () => {
+  throw new Error("Module not found");
+});
+
+import { IP2LocationAsnProvider } from "../../src/providers/asn/ip2location.provider";
+
+describe("IP2LocationAsnProvider", () => {
+  it("échoue si la lib n'est pas installée", async () => {
+    const p = new IP2LocationAsnProvider("./db/IP2LOCATION-LITE-ASN.BIN");
+
+    await expect(p.init()).rejects.toThrow(
+      "ip2location-nodejs n'est pas installé.",
+    );
+  });
+});
